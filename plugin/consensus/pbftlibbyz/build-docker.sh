40b3f917f83b19a9aa88ef8dd5da3c36ef7d3d3e
Gao <gaoc96@qq.com>
2018-12-14 21:54:15 +0800

    Update build-docker.sh

--- plugin/consensus/pbftlibbyz/build-docker.sh
+++ plugin/consensus/pbftlibbyz/build-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-dir=$(cd "$(dirname "$0")" || exit -1"; pwd)
-cd "$dir/../../../" || exit -1
+dir=$(cd "$(dirname "$0")" || "exit -1"; pwd)
+cd "${dir}/../../../" || "exit -1"
 docker build -t pbftlibbyz -f "$dir/plugin/consensus/pbftlibbyz/Dockerfile" .
